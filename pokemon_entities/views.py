52190ae6c709d99bdd3c8566ab6244e96128e82a
Vladimir <vladimir.papin@gmail.com>
2023-06-06 23:46:42 +0300

    deleted block if

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,7 +1,6 @@
 import folium
 
-from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Pokemon, PokemonEntity
 from django.utils.timezone import localtime
 
@@ -56,9 +55,7 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    pokemon = Pokemon.objects.get(id=pokemon_id)
-    if not pokemon:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+    pokemon = get_object_or_404(Pokemon, id=pokemon_id)
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     now_time = localtime()
